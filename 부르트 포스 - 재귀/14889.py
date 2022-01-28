4a971d209b5eb5e878ead9c0c0e626a49fbf0e47
parktj <ekswn120@gmail.com>
2022-01-28 20:27:32 +0900

    스타트와 링크 complete

--- 부르트 포스 - 재귀/14889.py
+++ 부르트 포스 - 재귀/14889.py
@@ -1,27 +1,28 @@
 # 스타트와 링크
 
 def go(index, first, second):
-    if index == n:
-        if len(first) != n//2:
+    if index == n:              # index는 현재 팀에 들어간 사람 수 이고 처음 정해놓은 인원수 n와 같지않을때까지
+        if len(first) != n//2:  # first팀에 들어간 인원이 n/2가 아니면 함수 빠져나오기
             return -1
-        if len(second) != n//2:
+        if len(second) != n//2: # second팀에 들어간 인원이 n/2가 아니면 함수 빠져나오기
             return -1
-        t1 = 0
-        t2 = 0
-        for i in range(n//2):
+        t1 = 0      # 첫번째 팀 능력치 합 0
+        t2 = 0      # 두번째 팀 능력치 합 0
+        for i in range(n//2):   
             for j in range(n//2):
                 if i == j:
                     continue
-                t1 += s[first[i]][first[j]]     # t1은 first팀의 능력치를 더한것
-                t2 += s[second[i]][second[j]]   # t2는 second팀의 능력치를 더한것
+                t1 += s[first[i]][first[j]]     # t1에 처음입력받은 i, j의 조합을 더해준다.
+                t2 += s[second[i]][second[j]]   # t2에 마찬가지로 더해준다. (i, j)의 조합을
         diff = abs(t1-t2)   # 두 팀의 능력치 차이를 빼고
         return diff         # 그 결과값을 리턴
     if len(first) > n//2:   # 한팀에 n의 반 인원보다 많이 들어갈 경우
         return -1           # 함수 빠져나오기
     if len(second) > n//2:  # first와 마찬가지
         return -1
+
     ans = -1
-    t1 = go(index+1, first+[index], second)
+    t1 = go(index+1, first+[index], second)     # first 배열에 index라는 인원 추가해서 재귀함수 호출
     if ans == -1 or (t1 != -1 and ans > t1):    # 능력치 차이의 최소값을 구하는거
         ans = t1                                # 
     t2 = go(index+1, first, second+[index])
