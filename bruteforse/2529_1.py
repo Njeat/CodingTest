66df75dca92661dd5e393f15e500cf89c37a9870
parktj <ekswn120@gmail.com>
2022-01-28 21:46:34 +0900

    부등호 complete

--- bruteforse/2529_1.py
+++ bruteforse/2529_1.py
@@ -1,24 +1,28 @@
 # 부등호
+# 5 < 9 > 1 < 3 < 4
+# 0 0 1 1 2 2 3 3 4  -> 숫자의 순서 따로 부등호의 순서 따로
+# '0'번째 숫자와 '1'번째 숫자를 비교하는 부등호는 '0'번째 부등호
+
 def ok(num):
-    for i in range(n):
-        if a[i] == '<':
-            if num[i] > num[i+1]:
-                return False
-        elif a[i] == '>':
+    for i in range(n):      
+        if a[i] == '<':     # i번째 부등호가 < 라면
+            if num[i] > num[i+1]:   # i번째 숫자와 i+1번째 숫자를 비교 하는데 부등호가 다르면
+                return False        # False 리턴
+        elif a[i] == '>':           # 마찬가지이다.
             if num[i] < num[i+1]:
                 return False
     
-    return True
+    return True     # if문에 한번도 걸리지 않았다면 정답이니까 True 리턴
 
-def go(index, num):
-    if index == n+1:
+def go(index, num):     #index는 숫자
+    if index == n+1:    # n(부등호의 개수)이 2라면 3개 숫자를 쓸수 있으니까
         if ok(num):
             ans.append(num)
         return
     for i in range(10):
-        if check[i]:
-            continue
-        check[i] = True
+        if check[i]:    # i번째 숫자가 사용안되었으면
+            continue    # 계속하고
+        check[i] = True # True로 바꿔주면서 사용했다고 바꿔줌
         go(index+1, num+str(i))
         check[i] = False
 
--- bruteforse/2529_2.py
+++ bruteforse/2529_2.py
@@ -1,4 +1,7 @@
 # 부등호
+# 6 < 5 과정까지 했을때 이미 정답에서 어긋나버려서 뒤에 작업을 하는것은 의미가 없지만
+# 2529_1에서는 그거까지 전부 다 계산하는 바람에 시간복잡도가 늘어나게 된다.
+# 2529_2에서는 그 과정을 없애면서 시간을 줄인다.
 
 def good(x, y, op):
     if op == '<':
@@ -16,7 +19,7 @@ def go(index, num):
     for i in range(10):
         if check[i]:
             continue
-        if index == 0 or good(num[index-1], str(i), a[index-1]):
+        if index == 0 or good(num[index-1], str(i), a[index-1]):    # 각 자리마다 부등호가 성립할때만 숫자를 넣어주는 코드 한줄 -> 시간복잡도가 확연히 줄어든다.
             check[i] = True
             go(index+1, num+str(i))
             check[i] = False
